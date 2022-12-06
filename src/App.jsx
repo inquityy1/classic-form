58a6bcf75e15b35e69aa1f53d7f27e5a9345bf41
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-12-06 17:11:01 +0100

    Just form with useState storage

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import './App.css'
+
+function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log('email:', email, 'password:', password);
+  }
+
+  return (
+    <div className="App">
+      <form style={{display:'flex', flexDirection:'column' }} onSubmit={onSubmit}>
+        <label htmlFor="Email">Email</label>
+        <input type="text" value={email} onChange={e => setEmail(e.target.value)} />
+        <label htmlFor="Password">Password</label>
+        <input type="text" value={password} onChange={e => setPassword(e.target.value)} />
+        <button style={{backgroundColor:'black', color:'white'}}>Click Me</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
